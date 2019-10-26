de89009e9ee7d35cc4071b40413b61ae9b727f1f
Ryan Dancy <ryan.dancy@uwaterloo.ca>
2019-10-26 10:01:42 -0400

    Add new patient button and list VBox
    There's now an "Add New Patient" button at the bottom of the ListPane
    and a VBox for storing patient summaries.

--- doctor-client/src/main/java/ca/uwaterloo/arka/pharmacy/ListController.java
+++ doctor-client/src/main/java/ca/uwaterloo/arka/pharmacy/ListController.java
@@ -1,8 +1,19 @@
 package ca.uwaterloo.arka.pharmacy;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
 /**
  * The controller class for the patient list on the left side.
  */
 public class ListController {
+    
+    @FXML private VBox patientList; // contains the graphical list of patients
+    
+    @FXML
+    void addNewPatient() {
+        // TODO
+        System.out.println("[ListController] Adding new patient");
+    }
     
 }
