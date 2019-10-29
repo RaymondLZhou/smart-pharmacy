e99c026e0890b5cd4a57438995118e939ebbaca3
Ryan Dancy <ryan.dancy@uwaterloo.ca>
2019-10-29 10:53:46 -0400

    Switch between editing and displaying modes
    DetailPane now swaps a bunch of controls when the edit/exit buttons
    are clicked.

--- doctor-client/src/main/java/ca/uwaterloo/arka/pharmacy/DetailController.java
+++ doctor-client/src/main/java/ca/uwaterloo/arka/pharmacy/DetailController.java
@@ -2,24 +2,45 @@ package ca.uwaterloo.arka.pharmacy;
 
 import ca.uwaterloo.arka.pharmacy.db.UserRecord;
 import javafx.beans.binding.Bindings;
-import javafx.beans.binding.StringBinding;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 import java.util.stream.Collectors;
 
 /**
- * The controller class for the "detail" pane on the right side.
+ * The controller class for the "detail" pane on the right side. Controls editing and saving records.
+ * Note: when we switch to editing mode, the old data is copied into the editing components, which are not bound
+ * to anything, unlike the displaying components, which are bound to the UserRecords' fields. The editing components
+ * then are modified and the old ones, along with the record, are saved upon hitting the save button and discarded
+ * upon hitting the exit button.
  */
+// TODO - a selection of doctors, also prescriptions rather than raw ID
 public class DetailController extends PaneController {
     
     @FXML private Node detailPaneRoot;
+    
+    @FXML private Button editBtn;
+    @FXML private Button deleteBtn;
+    @FXML private Button saveBtn;
+    @FXML private Button exitBtn;
     
     @FXML private Text nameText;
     @FXML private Text doctorsText;
     @FXML private Text prescriptionIdText;
     @FXML private Text faceFingerprintDataText;
+    
+    @FXML private TextField nameField;
+    @FXML private Node doctorsListContainer;
+    @FXML private ListView<String> doctorsList;
+    @FXML private Node prescriptionIdListContainer;
+    @FXML private ListView<Integer> prescriptionIdList;
+    @FXML private TextField faceFingerprintDataField;
+    
+    private boolean editing = false;
     
     private UserRecord record = null;
     
@@ -54,19 +75,77 @@ public class DetailController extends PaneController {
                 () -> record.getPrescriptions().stream()
                         .map(prescription -> Integer.toString(prescription.id))
                         .collect(Collectors.joining(", "))));
-
+        
         detailPaneRoot.setVisible(true);
     }
     
     @FXML
     private void edit() {
-        if (record == null) return;
-        // TODO
+        if (record == null || editing) return;
+        
+        // copy the record stuff into the editing stuff
+        nameField.setText(record.getName());
+        faceFingerprintDataField.setText(record.getFingerprint().getData());
+        doctorsList.getItems().clear();
+        // TODO have a better doctor choosing UI
+        doctorsList.getItems().addAll(record.getDoctors().stream().map(UserRecord.DoctorRecord::getName)
+                .collect(Collectors.toList()));
+        prescriptionIdList.getItems().clear();
+        // TODO have a list of prescriptions to choose from
+        prescriptionIdList.getItems().addAll(record.getPrescriptions().stream().map(prescription -> prescription.id)
+                .collect(Collectors.toList()));
+        
+        changeMode(true);
+    }
+    
+    @FXML
+    private void exit() {
+        if (record == null || !editing) return;
+        changeMode(false);
+    }
+    
+    private void changeMode(boolean edit) {
+        if (edit == editing) return;
+        editing = edit;
+        
+        // set all the displaying stuff to invisible if editing, visible if displaying
+        nameText.setManaged(!edit);
+        nameText.setVisible(!edit);
+        doctorsText.setManaged(!edit);
+        doctorsText.setVisible(!edit);
+        prescriptionIdText.setManaged(!edit);
+        prescriptionIdText.setVisible(!edit);
+        faceFingerprintDataText.setManaged(!edit);
+        faceFingerprintDataText.setVisible(!edit);
+        editBtn.setManaged(!edit);
+        editBtn.setVisible(!edit);
+        deleteBtn.setManaged(!edit);
+        deleteBtn.setVisible(!edit);
+        
+        // set all the editing stuff to visible if editing, invisible if displaying
+        nameField.setManaged(edit);
+        nameField.setVisible(edit);
+        doctorsListContainer.setManaged(edit);
+        doctorsListContainer.setVisible(edit);
+        prescriptionIdListContainer.setManaged(edit);
+        prescriptionIdListContainer.setVisible(edit);
+        faceFingerprintDataField.setManaged(edit);
+        faceFingerprintDataField.setVisible(edit);
+        saveBtn.setManaged(edit);
+        saveBtn.setVisible(edit);
+        exitBtn.setManaged(edit);
+        exitBtn.setVisible(edit);
     }
     
     @FXML
     private void delete() {
-        if (record == null) return;
+        if (record == null || editing) return;
+        // TODO
+    }
+    
+    @FXML
+    private void save() {
+        if (record == null || !editing) return;
         // TODO
     }
     
