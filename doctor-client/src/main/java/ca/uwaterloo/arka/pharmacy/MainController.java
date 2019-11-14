380f77f77ea5d68de4e0b933fbc60b55c1d2b128
Ryan Dancy <ryan.dancy@uwaterloo.ca>
2019-11-14 11:55:28 -0500

    Make list pane prettier
    CSS, yay.

--- doctor-client/src/main/java/ca/uwaterloo/arka/pharmacy/MainController.java
+++ doctor-client/src/main/java/ca/uwaterloo/arka/pharmacy/MainController.java
@@ -1,12 +1,16 @@
 package ca.uwaterloo.arka.pharmacy;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 
 /**
  * The controller for the main scene. UI is defined in MainScene.fxml. Exists mostly to connect the list and detail
  * pane controllers.
  */
 public class MainController {
+    
+    @FXML private Node rootNode;
     
     // Controller classes for the two panes
     @FXML private ListController listPaneController;
@@ -16,6 +20,9 @@ public class MainController {
     private void initialize() {
         listPaneController.setControllers(listPaneController, detailPaneController);
         detailPaneController.setControllers(listPaneController, detailPaneController);
+        
+        // set everything unfocused by default
+        Platform.runLater(() -> rootNode.requestFocus());
     }
     
 }
